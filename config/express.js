84ae7de929d640874ae0cfb34d391f6e3064717a
Steven Bell <thestevenbell@gmail.com>
2016-03-02 14:37:25 -0500

    removed reference to non-existant favicon

--- config/express.js
+++ config/express.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var glob = require('glob');
 
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
